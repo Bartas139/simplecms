c126c933d0738ac50133855dd6702e1f2f94e427
Bartas139 <41232648+Bartas139@users.noreply.github.com>
2019-05-19 22:09:26 +0200

    Role Management completed

---
+++ admin/delete_role.php
@@ -0,0 +1,24 @@
+<?php
+	
+# pripojeni do db
+require '../assets/db.php';
+
+# pristup jen pro prihlaseneho uzivatele
+require '../assets/login_required.php';
+# pristup jen s perm manage_role
+require '../assets/check_perm.php';
+
+//Pro pristup je potrebné opravnení manage_roles
+$access = perm ('manage_roles', $_SESSION['user_id']);
+
+if ($access == 0){die ('Chyba  403: Nemáte oprávnění pro přístup na tuto stránku');}
+
+
+
+$stmt = $db->prepare("DELETE FROM roles WHERE id=?");
+
+$stmt->execute(array($_GET['id']));
+
+
+
+header('Location: manage_roles.php');
--- admin/manage_roles.php
+++ admin/manage_roles.php
@@ -14,57 +14,96 @@ $access = perm ('manage_roles', $_SESSION['user_id']);
 if ($access == 0){die ('Chyba  403: Nemáte oprávnění pro přístup na tuto stránku');}
 
 if(!empty($_POST) && (@$_POST['action']=='update')){
+	$errors="";
+
 	//Pokud není zaškrtlé žádné pole, tak toto pole nevznikne a není možné s ním pracovat.
 	if (!isset($_POST['checkbox'])){
 		$_POST['checkbox'] = [];
 	}
 
-	//Výber všech perm přiřazených dané roli
-	$query = $db->prepare('SELECT perm_id FROM role_perm WHERE role_id = ?');
-	$query->execute(array($_POST["role"]));
-	$permissions = $query->fetchALL(PDO::FETCH_COLUMN, 0);
 
-	//Vložení vybraných do databáze, popřípadě ponechání původních
-	foreach($_POST['checkbox'] as $value) {
-		if (!in_array($value, $permissions)){
-			//INSERT VALUE TO role_perm  + role ID
-			$query = $db->prepare('INSERT INTO role_perm (role_id, perm_id) VALUES (?, ?)');
-			$query->execute(array($_POST["role"],$value));
+	//existuje už role?
+	$query = $db->prepare('SELECT name FROM roles WHERE name=? LIMIT 1');
+	$query->execute(array($_POST["name"]));
+	$roleexist = $query->fetchColumn();
+
+	if ($_POST["name"] != $_POST["rolename"]){
+		//todo zjistit jestli se nový název už nenacháí v DB, když ne update název a pak inserty, když ano error
+		if (empty($_POST["name"])){
+			$errors.="Kategorie musí mít název<br />";
+		} elseif (!empty($roleexist)) {
+			$errors.="Kategorie s tímto názvem už existuje<br />";
+		} else {
+			$query = $db->prepare('UPDATE roles SET name=? WHERE id=?');
+			$query->execute(array($_POST["name"], $_POST["role"]));
 		}
+
 	}
 
-	//Smazaní těch co nejsou zaškrtlé
-	$noncheck = array_diff($permissions, $_POST['checkbox']);
-	foreach($noncheck as $value) {
-		$stmt = $db->prepare("DELETE FROM role_perm WHERE role_id=? AND perm_id=?");
-		$stmt->execute(array($_POST['role'], $value));
+	if (empty($errors)){
+		//Výber všech perm přiřazených dané roli
+		$query = $db->prepare('SELECT perm_id FROM role_perm WHERE role_id = ?');
+		$query->execute(array($_POST["role"]));
+		$permissions = $query->fetchALL(PDO::FETCH_COLUMN, 0); //fetch do obyčejného array
+
+		//Vložení vybraných do databáze, popřípadě ponechání původních
+		foreach($_POST['checkbox'] as $value) {
+			if (!in_array($value, $permissions)){
+				//INSERT VALUE TO role_perm  + role ID
+				$query = $db->prepare('INSERT INTO role_perm (role_id, perm_id) VALUES (?, ?)');
+				$query->execute(array($_POST["role"],$value));
+			}
+		}
+
+		//Smazaní těch co nejsou zaškrtlé
+		$noncheck = array_diff($permissions, $_POST['checkbox']);
+		foreach($noncheck as $value) {
+			$stmt = $db->prepare("DELETE FROM role_perm WHERE role_id=? AND perm_id=?");
+			$stmt->execute(array($_POST['role'], $value));
+		}
+		header('Location: manage_roles.php');
 	}
-	header('Location: role_management.php');
 }
 
 if(!empty($_POST) && (@$_POST['action']=='create')){
+	$errors="";
+
 	//Pokud není zaškrtlé žádné pole, tak toto pole nevznikne a není možné s ním pracovat.
 	if (!isset($_POST['checkbox'])){
 		$_POST['checkbox'] = [];
 	}
-
-	//vložení role
-	$query = $db->prepare('INSERT INTO roles (name) VALUES (?)');
-	$query->execute(array($_POST["name"]));
-
-	//získání ID role, přes role name (přes last inserted nebezpečné)
-	$query = $db->prepare('SELECT id FROM Roles WHERE name=? LIMIT 1');
-	$query->execute(array($_POST["name"]));
-	$newid = $query->fetchColumn();
-
-	//Vložení vybraných do databáze
-	foreach($_POST['checkbox'] as $value) {
-			$query = $db->prepare('INSERT INTO role_perm (role_id, perm_id) VALUES (?, ?)');
-			$query->execute(array($newid,$value));
+	//Oveření chyb
+	if (empty($_POST['name'])){
+		$errors.="Kategorie musí mít název<br />";
 	}
 
+	$query = $db->prepare('SELECT name FROM roles WHERE name=? LIMIT 1');
+	$query->execute(array($_POST["name"]));
+	$roleexist = $query->fetchColumn();
+	if (!empty($roleexist)){
+		$errors.="Kategorie s názvem: ".$roleexist.", již existuje<br />";
+	}
 
-	header('Location: role_management.php');
+	if (empty($errors)){
+		//vložení role
+		$query = $db->prepare('INSERT INTO roles (name) VALUES (?)');
+		$query->execute(array($_POST["name"]));
+
+		//získání ID role, přes role name (přes last inserted nebezpečné)
+		$query = $db->prepare('SELECT id FROM Roles WHERE name=? LIMIT 1');
+		$query->execute(array($_POST["name"]));
+		$newid = $query->fetchColumn();
+
+		//Vložení vybraných do databáze
+		foreach($_POST['checkbox'] as $value) {
+				$query = $db->prepare('INSERT INTO role_perm (role_id, perm_id) VALUES (?, ?)');
+				$query->execute(array($newid,$value));
+		}
+
+
+		header('Location: manage_roles.php');
+	}
+	
 }
 
 ?><!DOCTYPE html>
@@ -83,7 +122,7 @@ if(!empty($_POST) && (@$_POST['action']=='create')){
 	<?php include '../navbar.php'; ?>
 	<div class="container">
 	<h1>Administrace rolí a přístupů</h1>
-
+	<?php echo (!empty($errors)?'<div class="alert alert-danger"><strong>'.$errors.'</strong></div>':'');?>
 	<?php
         $query = $db->prepare('SELECT * FROM roles ORDER BY name;');
         $query->execute();
@@ -91,43 +130,54 @@ if(!empty($_POST) && (@$_POST['action']=='create')){
         foreach ($roles as $role){
             
         	if ($role['name'] == 'SuperAdmin'){
-        		echo '<h2 id="' . htmlspecialchars($role["name"]) . '">'. htmlspecialchars($role["name"]) .'</h2>';
-        		echo 'Tato role má všechna oprávnění a nelze ji měnit';
+        		echo '<div class="card adminrole"><div class="card-header"><h2 id="' . htmlspecialchars($role["name"]) . '">'. htmlspecialchars($role["name"]) .'</h2></div>';
+        		echo '<div class="card-body">Tato role má všechna oprávnění a nelze ji měnit</div></div>';
         	} else {
-        		echo '<h2 id="' . htmlspecialchars($role["name"]) . '">'. htmlspecialchars($role["name"]) .'</h2>';
-        		echo '<form method="post">';
+        		echo '<div class="card adminrole"><div class="card-header"><h2 id="' . htmlspecialchars($role["name"]) . '">'. htmlspecialchars($role["name"]) .'</h2></div>';
+        		echo '<div class="card-body"><form method="post">';
         		echo '<input type="hidden" name="action" value="update" />';
         		echo '<input type="hidden" name="role" value="'.$role["id"].'" />';
-	            $query = $db->prepare('SELECT permissions.id, permissions.name FROM Permissions RIGHT JOIN role_perm ON permissions.id=role_perm.perm_id WHERE (role_perm.role_id=?)');
+        		echo '<input type="hidden" name="rolename" value="'.htmlspecialchars($role["name"]).'" />';
+				echo '<div class="form-group"><label for="name" >Upravte, pokud nechcete upravit název role:</label>';
+        		echo '<input class="form-control" type="text" id="name" name="name" value="'.htmlspecialchars($role["name"]).'" /></div>';
+	            $query = $db->prepare('SELECT permissions.id, permissions.name, permissions.description FROM Permissions RIGHT JOIN role_perm ON permissions.id=role_perm.perm_id WHERE (role_perm.role_id=?)');
 	        	$query->execute(array($role["id"]));
 	        	$permissions = $query->fetchALL(PDO::FETCH_ASSOC);
-	        	echo '<br />Aktuální oprávnění <br />';
+	        	echo '<h3>Aktuální oprávnění </h3>';
 	        	foreach ($permissions as $permission){
-		            echo '<input type="checkbox" value="' . $permission["id"] . '" name="checkbox[]" checked>'. $permission["name"] .'</input>';
+	        		echo '<div class="custom-control custom-checkbox mb-3">';
+		            echo '<input class="custom-control-input" id="'. $permission["id"] .'" type="checkbox" value="' . $permission["id"] . '" name="checkbox[]" checked />';
+		            echo '<label class="custom-control-label" for="'.$permission["id"].'" >'.htmlspecialchars($permission["description"]).'</label>';
+		            echo '</div>';
 	        	}
 	        	
 	        	$query = $db->prepare('SELECT * from Permissions WHERE (id, name) NOT IN (SELECT permissions.id, permissions.name FROM Permissions RIGHT JOIN role_perm ON permissions.id=role_perm.perm_id WHERE (role_perm.role_id=?))');
 	        	$query->execute(array($role["id"]));
 	        	$permissions = $query->fetchALL(PDO::FETCH_ASSOC);
-	        	echo '<br />Dostupná oprávnění <br />';
+	        	echo '<h3>Dostupná oprávnění </h3>';
 	        	foreach ($permissions as $permission){
-		            echo '<input type="checkbox" value="' . $permission["id"] . '" name="checkbox[]">'. $permission["name"] .'</input>';
+		            echo '<div class="custom-control custom-checkbox mb-3">';
+		            echo '<input class="custom-control-input" id="'. $permission["id"] .'" type="checkbox" value="' . $permission["id"] . '" name="checkbox[]" />';
+		            echo '<label class="custom-control-label" for="'.$permission["id"].'" >'.htmlspecialchars($permission["description"]).'</label>';
+		            echo '</div>';
 	        		}
-	        	echo '<input type="submit" value="Odeslat" />';	
-	        	echo '</form>';	
+	        	echo '</div><div class="card-footer"><input type="submit" value="Uložit" class="btn btn-primary send"/>';
+	        	echo '<a role="button" class="btn btn-primary send" href="delete_role.php?id='. $role["id"].'" onclick="return confirm(\'Přejete si smazat roli ' . htmlspecialchars($role['name']) . '\')">Smazat</a>';
+
+	        	echo '</form></div></div>';	
         		}
 
             
         }    
     ?>
-<h2>Vytvořit novou roli</h2>
+<div class="card adminrole"><div class="card-header"><h2>Vytvořit novou roli</h2></div><div class="card-body">
 <form method="post">
-                    <?php echo (!empty($chyby)?'<div class="alert alert-danger"><strong>'.$chyby.'</strong></div>':'');?>
+                    
       <input type="hidden" name="action" value="create" />
                    
                     <div class="form-group">
                         <label for="name">Název</label>
-                        <input type="text" name="name" id="name" class="form-control" value="<?php htmlspecialchars(@$_REQUEST['name']) ?>" required />
+                        <input type="text" name="name" id="name" class="form-control" value="<?php echo htmlspecialchars(@$_POST['name']) ?>" required />
                     </div>
                     <?php
 					$query = $db->prepare('SELECT * from Permissions');
@@ -135,14 +185,18 @@ if(!empty($_POST) && (@$_POST['action']=='create')){
 						        	$permissions = $query->fetchALL(PDO::FETCH_ASSOC);
 						        	echo '<br />Dostupná oprávnění <br />';
 						        	foreach ($permissions as $permission){
-							            echo '<input type="checkbox" value="' . $permission["id"] . '" name="checkbox[]">'. $permission["name"] .'</input>';
+						        		echo '<div class="custom-control custom-checkbox mb-3">';
+							            echo '<input class="custom-control-input" type="checkbox" value="' . $permission["id"] . '" name="checkbox[]" />';
+							            echo '<label class="custom-control-label" for="'.$permission["id"].'" >'.htmlspecialchars($permission["description"]).'</label>';
+							            echo '</div>';
 						        		}
                     ?>
+                </div><div class="card-footer">
                     <div class="form-group">
-                        <input type="submit" value="Odeslat" class="btn btn-primary"/>
+                        <input type="submit" value="Vytvořit" class="btn btn-primary send"/>
                     </div>    
                 </form>
-
+</div></div>
         </div>
 
 </div>
